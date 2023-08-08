c8bfeab3c1d7f02a54457f2c32ee7e31445c27d5
Kartikey Soni <sonikartikey005@gmail.com>
2023-08-07 23:18:58 -0400

    Updates

---
+++ precache-manifest.2bc4a94c8e4dfcaaae13914ff3e7ef65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ee22a3697fa4139b744fb61818d4002",
+    "url": "/kalyani_portfolio/index.html"
+  },
+  {
+    "revision": "8318d73b98aa49cc10ed",
+    "url": "/kalyani_portfolio/static/css/2.34178e39.chunk.css"
+  },
+  {
+    "revision": "061d21953d2d0ddca149",
+    "url": "/kalyani_portfolio/static/css/main.7db2b6dd.chunk.css"
+  },
+  {
+    "revision": "8318d73b98aa49cc10ed",
+    "url": "/kalyani_portfolio/static/js/2.aa58a3a5.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/kalyani_portfolio/static/js/2.aa58a3a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "061d21953d2d0ddca149",
+    "url": "/kalyani_portfolio/static/js/main.4cb98001.chunk.js"
+  },
+  {
+    "revision": "47f85a677cf78407dbbe",
+    "url": "/kalyani_portfolio/static/js/runtime-main.7c9fa89f.js"
+  }
+]);
